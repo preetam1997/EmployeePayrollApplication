6b4cf82a42ee9c4814c33fd5f334fbe667122f62
unknown <ompreetam317@gmail.com>
2020-11-19 07:55:05 +0530

    [Preetam]Added: Email Password and Phone Number validation

--- js/js-1.js
+++ js/js-1.js
@@ -13,3 +13,30 @@ text.addEventListener("input",function(){
         textError.textContent="";
     else textError.textContent = "Name is Incorrect";
 })
+
+const pwd = document.querySelector("#pwd");
+const pwdError = document.querySelector(".pwd-error");
+pwd.addEventListener("input",function(){
+    let pwdRegex = RegExp("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=[^.,:;'!@#$%^&*_+=|]*[.,:;'!@#$%^&*_+=|][^.,:;'!@#$%^&*_+=|]*$)[A-Za-z0-9.,:;'!@#$%^&*_+=|]{8,16}$");
+    if(pwdRegex.test(pwd.value))
+        pwdError.textContent="";
+    else pwdError.textContent = "Password is Incorrect";
+})
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector(".email-error");
+email.addEventListener("input",function(){
+    let emailRegex = RegExp("^(?!.*\\.{2})([a-zA-Z0-9]+)([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]?)([a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)@([a-z A-Z]{2,20}).([a-z A-Z]{2,4})(.[a-z]{2,8})?$");
+    if(emailRegex.test(email.value))
+        emailError.textContent="";
+    else emailError.textContent = "E-mail is Incorrect";
+})
+
+const tel = document.querySelector("#tel");
+const telError = document.querySelector(".tel-error");
+tel.addEventListener("input",function(){
+    let telRegex = RegExp("^([0-9]{2})(\\s)([0-9]{10})$");
+    if(telRegex.test(tel.value))
+        telError.textContent="";
+    else telError.textContent = "E-mail is Incorrect";
+})
