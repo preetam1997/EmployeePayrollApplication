558100b86a1dc5782bbbfd7d7a031ed8ad97200c
unknown <ompreetam317@gmail.com>
2020-11-20 07:21:06 +0530

    [Preetam]Added:  UC 8 Salary Event Listener

--- js/js_1.js
+++ js/js_1.js
@@ -1,3 +1,10 @@
+const salary = document.querySelector("#salary");
+const output = document.querySelector(".salary-output");
+output.textContent = salary.value;
+salary.addEventListener("input",function(){
+    output.textContent = salary.value;
+});
+
 
 
 
